2922234641bae73e3d6142ef3f3ccd7186b0d672
dursunalisevinc <sevincdursunali61@gmail.com>
2025-02-01 18:50:01 +0300

    edit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-    console.log("hello world");
-}
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+// Mıddlewares
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+})
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} unda başlatıldı.`);
+})
